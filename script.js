94268a365b117470b4a03bb64cd1d4e214bde0a1
s18629 <57294801+s18629@users.noreply.github.com>
2021-01-23 13:44:01 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const keys = document.querySelectorAll(".key"),
+  note = document.querySelector(".nowplaying");
+  
+
+function playNote(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`),
+    key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+  if (!key) return;
+  key.classList.add("playing");
+  audio.currentTime = 0;
+  audio.play();
+}
+
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return;
+  this.classList.remove("playing");
+}
+
+
+keys.forEach(key => key.addEventListener("transitionend", removeTransition));
+
+window.addEventListener("keydown", playNote);
